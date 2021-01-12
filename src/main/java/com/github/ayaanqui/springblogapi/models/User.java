8b79614974e50dc74ef055a516d6b335fd7a3c54
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2021-01-12 14:33:26 -0600

    Create User model

---
+++ src/main/java/com/github/ayaanqui/springblogapi/models/User.java
@@ -0,0 +1,46 @@
+package com.github.ayaanqui.springblogapi.models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String email;
+    private String username;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
