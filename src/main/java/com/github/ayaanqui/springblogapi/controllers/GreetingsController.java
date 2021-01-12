339cdef7cc7a3859f247bc8ee942519c40b4ec7b
ayaanqui <moahammedayaan.dev@gmail.com>
2021-01-12 13:31:01 -0600

    Add /greetings controller

---
+++ src/main/java/com/github/ayaanqui/springblogapi/controllers/GreetingsController.java
@@ -0,0 +1,35 @@
+package com.github.ayaanqui.springblogapi.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+public class GreetingsController {
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    private class Greeting {
+        private final long id;
+        private final String content;
+
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        public long getId() {
+            return this.id;
+        }
+
+        public String getContent() {
+            return this.content;
+        }
+    }
+
+    @GetMapping("/greetings")
+    public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String val) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, val));
+    }
+}
