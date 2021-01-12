33bdcc9ec37b87e0ff6d36d74121211994930e08
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2021-01-12 16:29:53 -0600

    Add /user controller

---
+++ src/main/java/com/github/ayaanqui/springblogapi/controllers/UsersController.java
@@ -0,0 +1,45 @@
+package com.github.ayaanqui.springblogapi.controllers;
+
+import com.github.ayaanqui.springblogapi.models.User;
+import com.github.ayaanqui.springblogapi.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequestMapping(path = "/users")
+public class UsersController {
+    @Autowired
+    private UserRepository userRepository;
+
+    private class Response {
+        private final String message;
+
+        public Response(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
+    @PostMapping("")
+    public @ResponseBody Response postNewUser(@RequestParam String name, @RequestParam String email,
+            @RequestParam String username) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setUsername(username);
+
+        userRepository.save(user);
+        return new Response("User created successfully!");
+    }
+
+    @GetMapping("")
+    public @ResponseBody Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
