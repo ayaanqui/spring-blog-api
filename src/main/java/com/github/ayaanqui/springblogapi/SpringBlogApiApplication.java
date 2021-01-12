2b82cc125a80bb205854c3177a71f18cab8fa152
ayaanqui <moahammedayaan.dev@gmail.com>
2021-01-12 12:19:18 -0600

    Initial commit

---
+++ src/main/java/com/github/ayaanqui/springblogapi/SpringBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.ayaanqui.springblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogApiApplication.class, args);
+	}
+
+}
