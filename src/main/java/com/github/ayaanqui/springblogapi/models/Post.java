a84e019a0956ff858e242600544ac9b279e33fa0
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2021-01-13 19:01:31 -0600

    Create post model

---
+++ src/main/java/com/github/ayaanqui/springblogapi/models/Post.java
@@ -0,0 +1,76 @@
+package com.github.ayaanqui.springblogapi.models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.*;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NonNull
+    private String title;
+
+    private String image;
+
+    @NonNull
+    private String content;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private Timestamp published;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getPublished() {
+        return published;
+    }
+
+    public void setPublished(Timestamp published) {
+        this.published = published;
+    }
+}
